7be8420de0a3c3fcff01fa129b70730c334cf089
User <User@LAB3-3>
2023-09-06 11:36:16 +0300

    update

--- CardGame/Deck.cs
+++ CardGame/Deck.cs
@@ -9,12 +9,39 @@ namespace CardGame
     public class Deck
     {
         private Card[] cards;
+        private int top;
+        Random rand;
 
-        public Deck() { }
+         public Deck()
+           
+        {
+            top = 0;
+            int index = 0; 
+            cards = new Card[52];
+            for(int i = 1; i <= 4; i++)
+            {
+               
+                for (int j = 1; j <= 13; j++)
+                { 
+                    cards[index] = new Card(j, i);
+                    index++;
+                }
+            }
+        }
         public Card Peek() { return null; }
-        public void Shuffle() { }
-        public Card Deal() { return null; }
+        public void Shuffle() 
+        {
 
-        public void Reset() { }
+        }
+        public Card Deal() 
+        { Card c = cards[top];
+            top++;
+            return c;
+        }
+
+        public void Reset() 
+        {
+            
+        }
     }
 }
--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -4,15 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Card c = new Card(1, 1);
-            Card c1 = new Card(2, 2);
-            Card c3= new Card(3, 3);    
-            Card c4= new Card(4, 4);
-            Console.WriteLine(c);
-            Console.WriteLine(c1);
-            Console.WriteLine(c3);
-            Console.WriteLine(c4);
-            
+            //Card c = new Card(1, 1);
+            //Card c1 = new Card(2, 2);
+            //Card c3= new Card(3, 3);    
+            //Card c4= new Card(4, 4);
+            //Console.WriteLine(c);
+            //Console.WriteLine(c1);
+            //Console.WriteLine(c3);
+            //Console.WriteLine(c4);
+            Deck deck = new Deck();
+            deck.Deal();//
         }
     }
 }
